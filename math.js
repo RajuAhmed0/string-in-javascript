d04c9b99fcd5dc1345d4663ff2721a79bb1069c3
Your Name <you@example.com>
2024-07-08 14:00:13 +0600

    math complate

--- math.js
+++ math.js
@@ -1,2 +1,28 @@
 // 7^9
-console.log(Math.pow(7, 9));
+// console.log(Math.pow(7, 9));
+// console.log(Math.abs(Math.random()*6));
+
+const asif = 98;
+const mehedi = 96;
+// console.log(Math.abs(asif - mehedi));
+// console.log(Math.abs(asif + mehedi));
+// console.log(Math.abs(asif * mehedi));
+// console.log(Math.abs(asif / mehedi));
+// console.log(Math.abs(asif % mehedi));
+
+// if (Math.abs(asif-mehedi) < 10) {
+//     console.log('tomer shate khelbo');
+// }
+// else{
+//     console.log('tumi amer bondhu na');
+// }
+
+const number = 4.58999999;
+// console.log(Math.round(number)); /* 5 ar soman ba 5 ar upore hole 1 num besi nibe ta sara prothom man nibe */
+// console.log(Math.ceil(number)); /* dosomic ar por 0 bade ja thakbe ta + kore uporer man dibe */
+// console.log(Math.floor(number)); /* dosomic ar pore ja thakbe sob bad  */
+
+// ludo concept
+console.log(Math.ceil(Math.random()*6));
+
+
---
+++ string.js
@@ -0,0 +1,26 @@
+const music = 'Amer mon mojaiya reeeaaa. dil mojaiya murshid nijerrr deshe jai. o murshidoooo ake amer vanga ghor tar opore lore kokhon jani ai ghor vainga porere';
+
+const searchWord = 'mon';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
